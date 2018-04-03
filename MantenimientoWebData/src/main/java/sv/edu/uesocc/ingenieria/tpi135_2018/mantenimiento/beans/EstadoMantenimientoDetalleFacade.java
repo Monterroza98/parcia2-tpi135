241ff0f992c1b0a02ae0a91533f2ed27d485c09c
jokerkr98 <kejoromo1998@gmail.com>
2018-04-03 03:03:37 +0000

    beans creados

---
+++ MantenimientoWebData/src/main/java/sv/edu/uesocc/ingenieria/tpi135_2018/mantenimiento/beans/EstadoMantenimientoDetalleFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.uesocc.ingenieria.tpi135_2018.mantenimiento.beans;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import sv.edu.uesocc.ingenieria.tpi135_2018.mantenimiento.entidades.EstadoMantenimientoDetalle;
+
+/**
+ *
+ * @author joker
+ */
+@Stateless
+public class EstadoMantenimientoDetalleFacade extends AbstractFacade<EstadoMantenimientoDetalle> {
+
+    @PersistenceContext(unitName = "com.mycompany_MantenimientoWebData_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public EstadoMantenimientoDetalleFacade() {
+        super(EstadoMantenimientoDetalle.class);
+    }
+    
+}
