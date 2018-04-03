0043eee1f550b94b7618bd79246f5d2d80f4574e
joker <joker@localhost>
2018-04-03 02:54:40 +0000

    entidades agregadas

---
+++ MantenimientoWebData/src/main/java/sv/edu/uesocc/ingenieria/tpi135_2018/mantenimiento/entidades/Equipo.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.uesocc.ingenieria.tpi135_2018.mantenimiento.entidades;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author joker
+ */
+@Entity
+@Table(name = "equipo")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Equipo.findAll", query = "SELECT e FROM Equipo e")
+    , @NamedQuery(name = "Equipo.findByIdEquipo", query = "SELECT e FROM Equipo e WHERE e.idEquipo = :idEquipo")
+    , @NamedQuery(name = "Equipo.findByCorrelativo", query = "SELECT e FROM Equipo e WHERE e.correlativo = :correlativo")
+    , @NamedQuery(name = "Equipo.findByObservaciones", query = "SELECT e FROM Equipo e WHERE e.observaciones = :observaciones")})
+public class Equipo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_equipo")
+    private Integer idEquipo;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "correlativo")
+    private String correlativo;
+    @Size(max = 2147483647)
+    @Column(name = "observaciones")
+    private String observaciones;
+    @OneToMany(mappedBy = "idEquipo")
+    private Collection<OrdenTrabajo> ordenTrabajoCollection;
+    @OneToMany(mappedBy = "idEquipo")
+    private Collection<EquipoDetalle> equipoDetalleCollection;
+
+    public Equipo() {
+    }
+
+    public Equipo(Integer idEquipo) {
+        this.idEquipo = idEquipo;
+    }
+
+    public Equipo(Integer idEquipo, String correlativo) {
+        this.idEquipo = idEquipo;
+        this.correlativo = correlativo;
+    }
+
+    public Integer getIdEquipo() {
+        return idEquipo;
+    }
+
+    public void setIdEquipo(Integer idEquipo) {
+        this.idEquipo = idEquipo;
+    }
+
+    public String getCorrelativo() {
+        return correlativo;
+    }
+
+    public void setCorrelativo(String correlativo) {
+        this.correlativo = correlativo;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    @XmlTransient
+    public Collection<OrdenTrabajo> getOrdenTrabajoCollection() {
+        return ordenTrabajoCollection;
+    }
+
+    public void setOrdenTrabajoCollection(Collection<OrdenTrabajo> ordenTrabajoCollection) {
+        this.ordenTrabajoCollection = ordenTrabajoCollection;
+    }
+
+    @XmlTransient
+    public Collection<EquipoDetalle> getEquipoDetalleCollection() {
+        return equipoDetalleCollection;
+    }
+
+    public void setEquipoDetalleCollection(Collection<EquipoDetalle> equipoDetalleCollection) {
+        this.equipoDetalleCollection = equipoDetalleCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idEquipo != null ? idEquipo.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Equipo)) {
+            return false;
+        }
+        Equipo other = (Equipo) object;
+        if ((this.idEquipo == null && other.idEquipo != null) || (this.idEquipo != null && !this.idEquipo.equals(other.idEquipo))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "sv.edu.uesocc.ingenieria.tpi135_2018.mantenimiento.entidades.Equipo[ idEquipo=" + idEquipo + " ]";
+    }
+    
+}
